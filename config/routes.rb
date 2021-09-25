1a5bbf53b13fe71b2daa8407c0d02d0a22601838
JAntonioLJ <chernpixel@gmail.com>
2021-09-25 16:50:10 -0500

    routes commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root_path = 'boards#index'
+
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users
+    resources :stars
+    resources :boards, only: :index
+    resources :dashboards, only: :index do
+    end
+  end
+
+  root to: root_path
+  get '/:locale', to: root_path
 end
